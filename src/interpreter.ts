01153173c5439e7a600f9453c75ba8f477bf19c1
Shashank AV <shashankav@ShashanAV-iMac.local>
2021-02-26 10:20:23 +0530

    initial commit

---
+++ src/interpreter.ts
@@ -0,0 +1,23 @@
+
+import React from "react";
+
+/**
+ * Helper class to interpet values
+ */
+class Interpreter extends React.Component {
+  visitor: any;
+  constructor(visitor) {
+    super(visitor);
+    this.visitor = visitor;
+  }
+
+  interpret(nodes) {
+    return this.visitor.run(nodes);
+  }
+
+  getValue() {
+    return this.visitor.value;
+  }
+}
+
+export default Interpreter;
